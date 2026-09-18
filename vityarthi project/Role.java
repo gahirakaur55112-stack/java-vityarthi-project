f92280b0ea6b788977cb7d993d43bc37e5297f95
Gahira Kaur <gahirakaur55112@gmail.com>
2026-09-18 22:43:55 +0530

    Add files via upload

---
+++ vityarthi project/Role.java
@@ -0,0 +1,15 @@
+enum Role {
+    STUDENT, ADMIN, STAFF
+}
+
+enum ComplaintCategory {
+    ELECTRICAL, PLUMBING, CLEANING, INTERNET, FURNITURE, WATER, OTHER
+}
+
+enum ComplaintPriority {
+    LOW, MEDIUM, HIGH, URGENT
+}
+
+enum ComplaintStatus {
+    OPEN, ASSIGNED, IN_PROGRESS, RESOLVED, CLOSED, CANCELLED
+}
